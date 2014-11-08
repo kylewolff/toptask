b4535d7b07df4902747a07492519794a1c2718c6
kylewolff <kyle.wolff7@gmail.com>
2014-11-08 10:42:48 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,63 @@
-<title>TopTask</title>
-Hello World
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    	<title>TopTask: To-Do List</title>
+        <link rel="stylesheet" type="text/css" href="stylesheets/crewStylesheet.css"/>
+        <script type="text/javascript" src="scripts/jquery-1.11.1.min.js"></script>
+        <script src="scripts/jquery-ui-1.11.1/jquery-ui.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="scripts/script.js"></script>
+</head>
+<body>
+<div class="sheet sheet--dark sheet--transparent sheet--with-bg sheet--hero sheet--padding" style="background-image: url(//d37hg21mvr93w7.cloudfront.net/img/hero-home.1411846737.jpg)">
+    <div class="container">
+        <div class="navbar navbar-transparent " role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#static-navigation-toggle"> <span class="sr-only">Toggle navigation</span>
+ <span class="icon-bar"></span>
+ <span class="icon-bar"></span>
+ <span class="icon-bar"></span>
+
+                    </button> <a href="/">
+                <h1 class="navbar-brand inline">
+                    <span class="logo">TOPTASK</span>
+                </h1>
+            </a>
+
+                </div>
+                <!-- close .navbar-header -->
+                <div class="collapse navbar-collapse" id="static-navigation-toggle">
+                    <ul class="nav navbar-nav">
+                        <div class="left logo"><a href="/rootToDo/index.php"><img src='/rootToDo/img/LogoTopTask.png' height="24"/></a></div>
+
+                        </li>
+                        
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="/rootToDo/include/php-login-advanced-master/index.php">Login</a>
+                        </li>
+                        <li><a href="/rootToDo/include/php-login-advanced-master/register.php" >Create Account</a>
+                            </li>
+                    </ul>
+                </div>
+                <!-- close .navbar-collapse -->
+            </div>
+            <!-- close .container-fluid -->
+        </div>
+        <!-- close .navbar -->
+        <div class="sheet__heading text-center">
+             <h1 class="giga text-heading-alt text-primary xs-margin-null xs-margin-bottom-small sm-margin-bottom-small md-margin-bottom-small">Rank your task list.</h1>
+
+            <div class="content--large xs-margin-bottom-large md-margin-bottom-large">
+                <p class="weight--normal xs-margin-null text-primary">Figure out what's most important to you. Finish it. Live on.</p>
+            </div> <a href="/rootToDo/include/php-login-advanced-master/index.php" class="btn btn-default">View Your List</a>
+
+        </div>
+        <!-- close .__content -->
+    </div>
+    <!-- close .container -->
+</div>
+ 
+</body>
+</html>
